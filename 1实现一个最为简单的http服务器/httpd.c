04e57150a8bc807a7409e07b4b6d3e73ec228534
lizhiyuan <148334733@qq.com>
2019-05-09 20:19:05 +0800

    去掉一些无意义的文件...

--- 1实现一个最为简单的http服务器/httpd.c
+++ 1实现一个最为简单的http服务器/httpd.c
@@ -30,4 +30,34 @@ fprintf(stderr,"[%s:%s:%d][error %d:%s]" fmt "\r\n",\
 #define _INT_BUF (1024)
 #define _INT_LIS (7)
 
+//读取文件描述符fd 一行的内容，保存在buf中，返回读取内容的长度
+int getfdline(int fd,char buf[],int sz);
+//返回400 请求解析失败,客户端代码错误
+extern inline void response_400(int cfd);
+//返回404 文件内容 请求文件没有找到
+extern inline void response_404(int cfd);
+//返回501 错误 不支持的请求
+extern inline void response_501(int cfd);
+//服务器内部错误，无法处理
+extern inline void response_500(int cfd);
+//返回200 请求成功 内容，后面可以加上其他的参数，处理文件输出
+extern inline void response_200(int cfd);
+
+//将文件发送给客户端
+void response_file(int cfd,const char *path);
+//返回启动服务器的描述符，这里灭有采用8080端口，防止冲突，用了随机端口
+int serstart(uint16_t pport);
+//在客户端链接过来，多线程处理的函数
+void* request_accept(void * arg);
+//处理客户端的http请求
+void request_cgi(int cfd,const char *path,const char*type,const char*query);
+//主逻辑，启动服务，可以做成守护进程
+int main(int argc,char *argv[]){
+    pthread_attr_t attr;
+    uint16_t port = 0;
+    int sfd = serstart(&port);
+
+
+}
+
 
